b0d7b35064efeeeb4c0d22eeb0c116301b8f495d
Sydney D. Abuyabor <sydneyabuyabor@gmail.com>
2024-02-05 23:26:44 +0800

    Moved makePage to TravelPage Widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:sample_flutter/animations/FadeAnimation.dart';
+import 'package:sample_flutter/widgets/TravelPage.dart';
 
 void main() => runApp(MaterialApp(
       debugShowCheckedModeBanner: false,
@@ -30,183 +31,40 @@ class _HomePageState extends State<HomePage> {
 
   void _onScroll() {}
 
-  Widget makePage({descrption, image, title, page}) {
-    return Container(
-      decoration: BoxDecoration(
-          image: DecorationImage(
-        image: AssetImage(image),
-        fit: BoxFit.cover,
-      )),
-      child: Container(
-        decoration: BoxDecoration(
-            gradient: LinearGradient(begin: Alignment.bottomRight, stops: [
-          0.3,
-          0.9
-        ], colors: [
-          Colors.black.withOpacity(.9),
-          Colors.black.withOpacity(.2),
-        ])),
-        child: Padding(
-          padding: EdgeInsets.all(20),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              SizedBox(
-                height: 20,
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.end,
-                crossAxisAlignment: CrossAxisAlignment.baseline,
-                textBaseline: TextBaseline.alphabetic,
-                children: [
-                  FadeAnimation(
-                    delay: 2,
-                    child: Text(page.toString(),
-                        style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 30,
-                            fontWeight: FontWeight.bold)),
-                  ),
-                  Text("/4",
-                      style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 15,
-                          fontWeight: FontWeight.bold)),
-                ],
-              ),
-              Expanded(
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.end,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    FadeAnimation(
-                      delay: 1,
-                      child: Text(
-                        title,
-                        style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 50,
-                            fontWeight: FontWeight.bold),
-                      ),
-                    ),
-                    SizedBox(
-                      height: 20,
-                    ),
-                    FadeAnimation(
-                      delay: 1.5,
-                      child: Row(
-                        children: [
-                          Container(
-                            margin: EdgeInsets.only(right: 3),
-                            child: Icon(
-                              Icons.star,
-                              color: Colors.yellow,
-                            ),
-                          ),
-                          Container(
-                            margin: EdgeInsets.only(right: 3),
-                            child: Icon(
-                              Icons.star,
-                              color: Colors.yellow,
-                            ),
-                          ),
-                          Container(
-                            margin: EdgeInsets.only(right: 3),
-                            child: Icon(
-                              Icons.star,
-                              color: Colors.yellow,
-                            ),
-                          ),
-                          Container(
-                            margin: EdgeInsets.only(right: 3),
-                            child: Icon(
-                              Icons.star,
-                              color: Colors.yellow,
-                            ),
-                          ),
-                          Container(
-                            margin: EdgeInsets.only(right: 3),
-                            child: Icon(
-                              Icons.star,
-                              color: Colors.grey,
-                            ),
-                          ),
-                          Text(
-                            "4.0",
-                            style: TextStyle(color: Colors.white70),
-                          ),
-                          Text(
-                            "(2300)",
-                            style:
-                                TextStyle(color: Colors.white38, fontSize: 12),
-                          ),
-                        ],
-                      ),
-                    ),
-                    SizedBox(height: 20),
-                    FadeAnimation(
-                      delay: 2.5,
-                      child: Padding(
-                        padding: const EdgeInsets.only(right: 100),
-                        child: Text(
-                          descrption,
-                          style: TextStyle(
-                            color: Colors.white.withOpacity(.7),
-                            height: 1.9,
-                          ),
-                        ),
-                      ),
-                    ),
-                    SizedBox(height: 20),
-                    FadeAnimation(
-                      delay: 2.8,
-                      child: Text(
-                        "READ MORE",
-                        style: TextStyle(color: Colors.white),
-                      ),
-                    ),
-                    SizedBox(height: 30),
-                  ],
-                ),
-              )
-            ],
-          ),
-        ),
-      ),
-    );
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: PageView(
         controller: _pageController,
         children: <Widget>[
-          makePage(
-              page: 1,
-              descrption:
-                  "Yosemite National Park is in California’s Sierra Nevada mountains. It’s famed for its giant, ancient sequoia trees, and for Tunnel View, the iconic vista of towering Bridalveil Fall and the granite cliffs of El Capitan and Half Dome.",
-              title: "Yosemite National Park",
-              image: 'assets/images/one.jpg'),
-          makePage(
-              page: 2,
-              descrption:
-                  "The Golden Gate Bridge is a suspension bridge spanning the Golden Gate, the one-mile-wide strait connecting San Francisco Bay and the Pacific Ocean.",
-              title: "Golden Gate Bridge",
-              image: 'assets/images/two.jpg'),
-          makePage(
+          TravelPage(
+            page: 1,
+            title: "Yosemite National Park",
+            image: 'assets/images/one.jpg',
+            description:
+                "Yosemite National Park is in California’s Sierra Nevada mountains. It’s famed for its giant, ancient sequoia trees, and for Tunnel View, the iconic vista of towering Bridalveil Fall and the granite cliffs of El Capitan and Half Dome.",
+          ),
+          TravelPage(
+            page: 2,
+            title: "Golden Gate Bridge",
+            image: 'assets/images/two.jpg',
+            description:
+                " The Golden Gate Bridge is a suspension bridge spanning the Golden Gate, the one-mile-wide strait connecting San Francisco Bay and the Pacific Ocean.",
+          ),
+          TravelPage(
             page: 3,
-            descrption:
-                "Sedona is regularly described as one of America's most beautiful places. Nowhere else will you find a landscape as dramatically colorful.",
             title: "Sedona",
             image: 'assets/images/three.jpg',
+            description:
+                "Sedona is regularly described as one of America's most beautiful places. Nowhere else will you find a landscape as dramatically colorful.",
           ),
-          makePage(
-              page: 4,
-              descrption:
-                  "Savannah, with its Spanish moss, Southern accents and creepy graveyards, is a lot like Charleston, South Carolina. But this city about 100 miles to the south has an eccentric streak.",
-              title: "Savannah",
-              image: 'assets/images/four.jpg'),
+          TravelPage(
+            page: 4,
+            title: "Savannah",
+            image: 'assets/images/four.jpg',
+            description:
+                "Savannah, with its Spanish moss, Southern accents and creepy graveyards, is a lot like Charleston, South Carolina. But this city about 100 miles to the south has an eccentric streak.",
+          ),
         ],
       ),
     );
--- lib/widgets/TravelPage.dart
+++ lib/widgets/TravelPage.dart
@@ -1,10 +1,170 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
-class TravelPage extends StatelessWidget {
-  const TravelPage({super.key});
+import '../animations/FadeAnimation.dart';
+
+class TravelPage extends StatefulWidget {
+  final int page;
+  final String description;
+  final String title;
+  final String image;
+
+  const TravelPage(
+      {super.key,
+      required this.page,
+      required this.description,
+      required this.title,
+      required this.image});
 
   @override
+  State<TravelPage> createState() => _TravelPageState();
+}
+
+class _TravelPageState extends State<TravelPage> {
+  @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Container(
+      decoration: BoxDecoration(
+          image: DecorationImage(
+        image: AssetImage(widget.image),
+        fit: BoxFit.cover,
+      )),
+      child: Container(
+        decoration: BoxDecoration(
+            gradient: LinearGradient(begin: Alignment.bottomRight, stops: [
+          0.3,
+          0.9
+        ], colors: [
+          Colors.black.withOpacity(.9),
+          Colors.black.withOpacity(.2),
+        ])),
+        child: Padding(
+          padding: EdgeInsets.all(20),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              SizedBox(
+                height: 20,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                crossAxisAlignment: CrossAxisAlignment.baseline,
+                textBaseline: TextBaseline.alphabetic,
+                children: [
+                  FadeAnimation(
+                    delay: 2,
+                    child: Text(widget.page.toString(),
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 30,
+                            fontWeight: FontWeight.bold)),
+                  ),
+                  Text("/4",
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 15,
+                          fontWeight: FontWeight.bold)),
+                ],
+              ),
+              Expanded(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    FadeAnimation(
+                      delay: 1,
+                      child: Text(
+                        widget.title,
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 50,
+                            fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    FadeAnimation(
+                      delay: 1.5,
+                      child: Row(
+                        children: [
+                          Container(
+                            margin: EdgeInsets.only(right: 3),
+                            child: Icon(
+                              Icons.star,
+                              color: Colors.yellow,
+                            ),
+                          ),
+                          Container(
+                            margin: EdgeInsets.only(right: 3),
+                            child: Icon(
+                              Icons.star,
+                              color: Colors.yellow,
+                            ),
+                          ),
+                          Container(
+                            margin: EdgeInsets.only(right: 3),
+                            child: Icon(
+                              Icons.star,
+                              color: Colors.yellow,
+                            ),
+                          ),
+                          Container(
+                            margin: EdgeInsets.only(right: 3),
+                            child: Icon(
+                              Icons.star,
+                              color: Colors.yellow,
+                            ),
+                          ),
+                          Container(
+                            margin: EdgeInsets.only(right: 3),
+                            child: Icon(
+                              Icons.star,
+                              color: Colors.grey,
+                            ),
+                          ),
+                          Text(
+                            "4.0",
+                            style: TextStyle(color: Colors.white70),
+                          ),
+                          Text(
+                            "(2300)",
+                            style:
+                                TextStyle(color: Colors.white38, fontSize: 12),
+                          ),
+                        ],
+                      ),
+                    ),
+                    SizedBox(height: 20),
+                    FadeAnimation(
+                      delay: 2.5,
+                      child: Padding(
+                        padding: const EdgeInsets.only(right: 100),
+                        child: Text(
+                          widget.description,
+                          style: TextStyle(
+                            color: Colors.white.withOpacity(.7),
+                            height: 1.9,
+                          ),
+                        ),
+                      ),
+                    ),
+                    SizedBox(height: 20),
+                    FadeAnimation(
+                      delay: 2.8,
+                      child: Text(
+                        "READ MORE",
+                        style: TextStyle(color: Colors.white),
+                      ),
+                    ),
+                    SizedBox(height: 30),
+                  ],
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
