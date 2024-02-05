100c4d6e31e4eb0da11ef2192027bcc294322ac7
Sydney D. Abuyabor <sydneyabuyabor@gmail.com>
2024-02-05 18:27:41 +0800

    Implemented text, image, and description

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,9 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(
-  MaterialApp(
-    debugShowCheckedModeBanner: false,
-    home: HomePage(),
-  )
-);
+void main() => runApp(MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+    ));
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -16,69 +14,143 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   late PageController _pageController;
+  late int _pageNum;
 
-@override
+  @override
   void initState() {
     // TODO: implement initState
-  _pageController = PageController(
+
+    _pageController = PageController(
       initialPage: 0,
-  )..addListener(_onScroll);
+    )..addListener(_onScroll);
     super.initState();
   }
 
-  void _onScroll(){
-  print("sss");
-  }
+  void _onScroll() {}
 
-  Widget makePage({image}){
+  Widget makePage({descrption, image, title}) {
     return Container(
       decoration: BoxDecoration(
-        image: DecorationImage(
-          image:AssetImage(image),
-          fit: BoxFit.cover,
-        )
-      ),
+          image: DecorationImage(
+        image: AssetImage(image),
+        fit: BoxFit.cover,
+      )),
       child: Container(
         decoration: BoxDecoration(
-          gradient: LinearGradient(
-            begin: Alignment.bottomRight,
-            stops: [0.3, 0.9],
-            colors: [
-              Colors.black.withOpacity(.9),
-              Colors.black.withOpacity(.2),
-            ]
-          )
-        ),
+            gradient: LinearGradient(begin: Alignment.bottomRight, stops: [
+          0.3,
+          0.9
+        ], colors: [
+          Colors.black.withOpacity(.9),
+          Colors.black.withOpacity(.2),
+        ])),
         child: Padding(
           padding: EdgeInsets.all(20),
           child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: <Widget>[
-              SizedBox(height: 20,),
+              SizedBox(
+                height: 20,
+              ),
               Row(
                 mainAxisAlignment: MainAxisAlignment.end,
                 crossAxisAlignment: CrossAxisAlignment.baseline,
                 textBaseline: TextBaseline.alphabetic,
                 children: [
-                  Text("1", style: TextStyle(color: Colors.white, fontSize: 30, fontWeight: FontWeight.bold)),
-                  Text("/4", style: TextStyle(color: Colors.white, fontSize: 15, fontWeight: FontWeight.bold)),
+                  Text("1",
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 30,
+                          fontWeight: FontWeight.bold)),
+                  Text("/4",
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 15,
+                          fontWeight: FontWeight.bold)),
                 ],
-
               ),
-              Expanded(child: Column(
-                mainAxisAlignment: MainAxisAlignment.end,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text("USA", style: TextStyle(color: Colors.white, fontSize: 50, fontWeight: FontWeight.bold)),
-                ],
-              ))
+              Expanded(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      title,
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 50,
+                          fontWeight: FontWeight.bold),
+                    ),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    Row(
+                      children: [
+                        Container(
+                          margin: EdgeInsets.only(right: 3),
+                          child: Icon(
+                            Icons.star,
+                            color: Colors.yellow,
+                          ),
+                        ),
+                        Container(
+                          margin: EdgeInsets.only(right: 3),
+                          child: Icon(
+                            Icons.star,
+                            color: Colors.yellow,
+                          ),
+                        ),
+                        Container(
+                          margin: EdgeInsets.only(right: 3),
+                          child: Icon(
+                            Icons.star,
+                            color: Colors.yellow,
+                          ),
+                        ),
+                        Container(
+                          margin: EdgeInsets.only(right: 3),
+                          child: Icon(
+                            Icons.star,
+                            color: Colors.yellow,
+                          ),
+                        ),
+                        Container(
+                          margin: EdgeInsets.only(right: 3),
+                          child: Icon(
+                            Icons.star,
+                            color: Colors.grey,
+                          ),
+                        ),
+                        Text(
+                          "4.0",
+                          style: TextStyle(color: Colors.white70),
+                        ),
+                        Text(
+                          "(2300)",
+                          style: TextStyle(color: Colors.white38, fontSize: 12),
+                        ),
+                      ],
+                    ),
+                    SizedBox(height: 20),
+                    Padding(
+                      padding: const EdgeInsets.only(right: 50),
+                      child: Text(
+                        descrption,
+                        style: TextStyle(
+                          color: Colors.white.withOpacity(.7),
+                          height: 1.9,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              )
             ],
-
           ),
         ),
       ),
     );
   }
-
 
   @override
   Widget build(BuildContext context) {
@@ -86,10 +158,27 @@ class _HomePageState extends State<HomePage> {
       body: PageView(
         controller: _pageController,
         children: <Widget>[
-          makePage(image: 'assets/images/one.jpg'),
-          makePage(image: 'assets/images/two.jpg'),
-          makePage(image: 'assets/images/three.jpg'),
-          makePage(image: 'assets/images/four.jpg'),
+          makePage(
+              descrption:
+                  "Yosemite National Park is in California’s Sierra Nevada mountains. It’s famed for its giant, ancient sequoia trees, and for Tunnel View, the iconic vista of towering Bridalveil Fall and the granite cliffs of El Capitan and Half Dome.",
+              title: "Yosemite National Park",
+              image: 'assets/images/one.jpg'),
+          makePage(
+              descrption:
+                  "The Golden Gate Bridge is a suspension bridge spanning the Golden Gate, the one-mile-wide strait connecting San Francisco Bay and the Pacific Ocean.",
+              title: "Golden Gate Bridge",
+              image: 'assets/images/two.jpg'),
+          makePage(
+            descrption:
+                "Sedona is regularly described as one of America's most beautiful places. Nowhere else will you find a landscape as dramatically colorful.",
+            title: "Sedona",
+            image: 'assets/images/three.jpg',
+          ),
+          makePage(
+              descrption:
+                  "Savannah, with its Spanish moss, Southern accents and creepy graveyards, is a lot like Charleston, South Carolina. But this city about 100 miles to the south has an eccentric streak.",
+              title: "Savannah",
+              image: 'assets/images/four.jpg'),
         ],
       ),
     );
